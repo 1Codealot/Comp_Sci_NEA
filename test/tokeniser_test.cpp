ef116f688a7e1f65780cd9a92904c2ac3073a575
1Codealot <58225804+1Codealot@users.noreply.github.com>
2025-12-18 09:59:30 +0000

    Added comments to tokeniser_test

--- test/tokeniser_test.cpp
+++ test/tokeniser_test.cpp
@@ -5,13 +5,13 @@
 
 int main(int argc, char const *argv[])
 {
+    // For taking in the file via the command line
     if (argc != 2)
     {
         std::cout << "Invalid usage.\n";
         return 1;
     }
 
-    std::cout << argv[1] << "\n";
     std::ifstream code_in(argv[1]);
     std::string code((std::istreambuf_iterator<char>(code_in)), std::istreambuf_iterator<char>());
 
@@ -23,11 +23,11 @@ int main(int argc, char const *argv[])
 
     for (std::string token : tokens)
     {
-        if (token == "\n")
+        if (token == "\n") // To make the tokens easier to read
         {
             token = "\\n";
         }
-        std::cout << "[" << token << "] ";
+        std::cout << "[" << token << "] "; // Once again making the tokens easier to read.
     }
 
     std::cout << std::endl;
